186bbf20f5e3e6cc684c44ce6f3afd519a94909f
fengxiaowei <fengxiaowei6@wanda.cn>
2017-09-25 15:29:27 +0800

    初次提交

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/page/index'
+import Content from '@/page/content'
+import UET from '@/page/ue_test'
+import JQ from '@/page/jq'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Index
+    }, {
+      path: '/content/:id',
+      component: Content
+    }, {
+      path: '/ue',
+      component: UET
+    }, {
+      path: '/jq',
+      component: JQ
+    }
+  ]
+})
